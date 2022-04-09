50afe39d615deb7fcd8927407a2441cbc06c1101
U-UL\marotta12u <marotta12u@iutm-inf-e37-21.iutmetz.site.univ-lorraine.fr>
2022-04-09 11:01:31 +0200

    add files

--- connexion.php
+++ connexion.php
@@ -3,6 +3,7 @@
     define('DB_USER', '');
     define('DB_PASSWORD', '');
     define('DB_HOST', 'localhost');
+    
 
     try{
         $pdo = new PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
