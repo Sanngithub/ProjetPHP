dd1cc71fad3db1ffa74e6bfaee488b045a58b8eb
U-UL\marotta12u <marotta12u@iutm-inf-e37-21.iutmetz.site.univ-lorraine.fr>
2022-04-09 10:54:11 +0200

    add files

---
+++ connexion.php
@@ -0,0 +1,15 @@
+<?php
+    define('DB_NAME', 'superfilms');
+    define('DB_USER', '');
+    define('DB_PASSWORD', '');
+    define('DB_HOST', 'localhost');
+
+    try{
+        $pdo = new PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
+    catch(Exception $e){
+        die('Erreur : '.$e->getMessage());
+    }
+?> 
