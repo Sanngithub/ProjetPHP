bc3497653eeb5fce77ba93b834ecbd8f1bcc738a
JonathanMAROTTA <exrazhell@yahoo.fr>
2022-05-14 13:51:43 +0200

    synchronisation informations connexion Bdd >>>ok

--- controleur/connexion.php
+++ controleur/connexion.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_NAME', 'superanimes');
-    define('DB_USER', 'superanimesUser');
-    define('DB_PASSWORD', '123456');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
     define('DB_HOST', 'localhost');
 
     try{
